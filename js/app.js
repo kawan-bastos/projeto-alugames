fea172d95889711d64a6ed1535b1a38085492590
Kawan Bastos <kawanbastos021@gmail.com>
2025-12-06 22:52:39 -0300

    adicionando efeito disable na img

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 function alterarStatus(id) {
     let game = document.getElementById(`game-${id}`);
     let botao = game.querySelector('.dashboard__item__button');
+    let img = game.querySelector('.dashboard__item__img');
 
     if (botao.innerHTML === "Alugar"){
         botao.innerHTML = "Devolver";
         botao.classList.add('dashboard__item__button--return');
+        img.classList.add('dashboard__item__img--rented');
     } else {
         botao.innerHTML = "Alugar";
         botao.classList.remove('dashboard__item__button--return');
+        img.classList.remove('dashboard__item__img--rented');
         
     }
 }
